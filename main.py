318028de0f7fe5995c8330eff1c2595eab40d457
Halo <68120801+TheHaloDeveloper@users.noreply.github.com>
2025-02-14 17:41:11 -0800

    avoiding ratelimits + saving to file

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 import itertools
 import requests
+import time
 
 chars = "abcdefghijklmnopqrstuvwxyz1234567890"
 combos = itertools.product(chars, repeat=4)
-count = 0
 
 for combo in combos:
     user = ''.join(combo)
     print(user)
-    count += 1
     
-    response = requests.post(
-        'https://users.roblox.com/v1/usernames/users',
-        json={
-            "usernames": [user],
-            "excludeBannedUsers": False
-        }
-    )
-    data = response.json()
-    print(data)
+    while True:
+        response = requests.post(
+            'https://users.roblox.com/v1/usernames/users',
+            json={
+                "usernames": [user],
+                "excludeBannedUsers": False
+            }
+        )
+        data = response.json()
+        
+        if "errors" in data:
+            for error in data["errors"]:
+                if error["message"] == "Too many requests":
+                    print("Too many requests")
+                    time.sleep(5)
+                    continue
+        break
     
-    if len(data['data']) == 0:
-        print('FOUND')
-        exit(0)
-    
-print(f"\n\n{count}")
+    if len(data.get('data', [])) == 0:
+        with open("unclaimed.txt", "a") as f:
+            f.write(user + "\n")
+            print("UNCLAIMED!")
