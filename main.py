2c10b19f6bfca9e1d8fc0491dfa74bd39ac42b3c
Halo <68120801+TheHaloDeveloper@users.noreply.github.com>
2025-02-14 17:36:56 -0800

    basic username checking

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import itertools
+import requests
 
 chars = "abcdefghijklmnopqrstuvwxyz1234567890"
 combos = itertools.product(chars, repeat=4)
 count = 0
 
 for combo in combos:
-    print(''.join(combo))
+    user = ''.join(combo)
+    print(user)
     count += 1
+    
+    response = requests.post(
+        'https://users.roblox.com/v1/usernames/users',
+        json={
+            "usernames": [user],
+            "excludeBannedUsers": False
+        }
+    )
+    data = response.json()
+    print(data)
+    
+    if len(data['data']) == 0:
+        print('FOUND')
+        exit(0)
     
 print(f"\n\n{count}")
