630f8ee2ca832cca47628b0703ad2e418710fade
Halo <68120801+TheHaloDeveloper@users.noreply.github.com>
2025-02-14 17:58:26 -0800

    nvm no random

--- main.py
+++ main.py
@@ -5,38 +5,32 @@ import random
 
 s = "abcdefghijklmnopqrstuvwxyz"
 chars = ''.join(random.sample(s, len(s)))
+combos = itertools.product(chars, repeat=6)
 
-def get_shuffled_combos():
-    combos = list(itertools.product(chars, repeat=6))
-    random.shuffle(combos)
-    for i in range(0, len(combos), 10000):
-        yield combos[i:i+10000]
+for combo in combos:
+    user = ''.join(combo)
+    print(user)
+    
+    while True:
+        response = requests.post(
+            'https://users.roblox.com/v1/usernames/users',
+            json={
+                "usernames": [user],
+                "excludeBannedUsers": False
+            }
+        )
+        data = response.json()
 
-for batch in get_shuffled_combos():
-    for combo in batch:
-        user = ''.join(combo)
-        print(user)
+        if "errors" in data:
+            if any(error["message"] == "Too many requests" for error in data["errors"]):
+                print("Too many requests")
+                time.sleep(5)
+                continue
+        else:
+            break 
 
-        while True:
-            response = requests.post(
-                'https://users.roblox.com/v1/usernames/users',
-                json={
-                    "usernames": [user],
-                    "excludeBannedUsers": False
-                }
-            )
-            data = response.json()
-
-            if "errors" in data:
-                if any(error["message"] == "Too many requests" for error in data["errors"]):
-                    print("Too many requests")
-                    time.sleep(5)
-                    continue
-            else:
-                break  
-
-        if "data" in data and len(data["data"]) == 0:
-            with open("unclaimed.txt", "a") as f:
-                f.write(user + "\n")
-                f.flush()
-            print("UNCLAIMED!")
+    if "data" in data and len(data["data"]) == 0:
+        with open("unclaimed.txt", "a") as f:
+            f.write(user + "\n")
+            f.flush()
+        print("UNCLAIMED!")
