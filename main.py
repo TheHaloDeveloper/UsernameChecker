71070ebcc65349b710fd9064f4c20c56e262d9d6
Halo <68120801+TheHaloDeveloper@users.noreply.github.com>
2025-02-14 17:28:09 -0800

    all combinations

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("init")
+import itertools
+
+chars = "abcdefghijklmnopqrstuvwxyz1234567890"
+combos = itertools.product(chars, repeat=4)
+count = 0
+
+for combo in combos:
+    print(''.join(combo))
+    count += 1
+    
+print(f"\n\n{count}")
